14c9bc3ad7db8b9823bfae302450d1bbcf6aeccc
Adria Sanchez <adrialol68@gmail.com>
2021-04-21 14:02:43 +0200

    Changed some things in capretassass.sh

--- carpetassass.sh
+++ carpetassass.sh
@@ -15,12 +15,14 @@ touch main.scss;
 echo "// IMPORTS
 
 //UTILITIES
-//@import '01_utilities/variables';
-//@import '01_utilities/functions';
-//@import '01_utilities/mixins';
+@import '01_utilities/variables';
+@import '01_utilities/functions';
+@import '01_utilities/mixins';
 
 //BASE
-//@import '02_base/reset';
+@import '02_base/box-sizing';
+@import '02_base/normalize';
+@import '02_base/reset';
 
 
 //LAYOUTS
@@ -44,6 +46,65 @@ touch _variables.scss;
 cd ..;
 
 cd 02_base;
+
 touch _box-sizing.scss;
-touch _reset1.scss;
-touch _reset2.scss;
+
+echo "html {
+    box-sizing: border-box;
+  }
+   
+  *, *:before, *:after {
+    box-sizing: inherit;
+    }" >> _box-sizing.scss;
+
+touch _normalize.scss;
+
+touch _reset.scss;
+
+echo "/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}" >> __reset.scss;
